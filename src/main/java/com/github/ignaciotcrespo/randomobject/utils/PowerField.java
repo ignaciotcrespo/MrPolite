291e2ab47bf0e9c84369489522cee1b104887b7d
ignaciotcrespo <itcrespo@gmail.com>
2017-03-06 21:10:49 -0300

    added travis-ci

--- src/main/java/com/github/ignaciotcrespo/randomobject/utils/PowerField.java
+++ src/main/java/com/github/ignaciotcrespo/randomobject/utils/PowerField.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License
+ * <p>
+ * Copyright (c) 2017, Ignacio Tomas Crespo (itcrespo@gmail.com)
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.github.ignaciotcrespo.randomobject.utils;
 
 import com.github.ignaciotcrespo.randomobject.GeneratedValue;
@@ -57,7 +80,9 @@ public class PowerField {
             if (genericTypes != null && genericTypes.length == parentClass.getGenerics().length) {
                 for (int i = 0; i < genericTypes.length; i++) {
                     Type genericType = genericTypes[i];
-                    if (genericType.getTypeName().equals(fieldGenericType.getTypeName())) {
+                    String genericName = getTypeName(genericType);
+                    String fieldGenericName = getTypeName(fieldGenericType);
+                    if (genericName.equals(fieldGenericName)) {
                         Type type = parentClass.getGenerics()[i];
                         return getClassFromType(type);
                     }
@@ -65,6 +90,20 @@ public class PowerField {
             }
         }
         return field.getType();
+    }
+
+    private String getTypeName(Type type) {
+        // jdk 1.6 doesnt support type.getTypeName()
+        if (type instanceof Class) {
+            return ((Class) type).getName();
+        } else if (type instanceof TypeVariable) {
+            return ((TypeVariable) type).getName();
+        } else if (type instanceof GenericArrayType) {
+            return getTypeName(((GenericArrayType) type).getGenericComponentType());
+        } else if (type instanceof ParameterizedType) {
+            return getTypeName(((ParameterizedType) type).getRawType());
+        }
+        throw new MrPoliteException("Type not supported yet! " + type);
     }
 
     private Class<?> getClassFromType(Type type) {
